ee1e8cb6a470c274b775801cc57efbe5ca452a5d
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 18:49:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 def find_even_values(src)
   row_index = 0
-  even_numbers = 0
+  even_numbers = []
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
